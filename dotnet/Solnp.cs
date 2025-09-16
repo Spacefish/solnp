afee6a4c463f377429fb909e0034dbcec5468dc1
Timo Witte <timo_witte@agilent.com>
2025-09-16 15:39:33 +0200

    add empty C# implementation

---
+++ dotnet/Solnp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace dotnet;
+
+public struct SolveResult
+{
+    double SolveValue;
+    double[] Optimum;
+    bool Converged;
+    double[,] HessianMatrix;
+};
+
+public static class Solnp
+{
+    /// <summary>
+    /// Todo: migrate parameters and implementation from c library
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public static SolveResult solnp()
+    {
+        throw new NotImplementedException();
+    }
+}
